ad0014b69920630f957b6962556c3bd9e1a76cd8
nathaniellamnl <nl.nathanlam@gmail.com>
2020-10-01 11:21:18 +0800

    Updates

---
+++ precache-manifest.582f83ee86428744f9aa855af2ea4348.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b512acb69dd66d5c0103f53950a97bd",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "c826c36fea8f693adde9",
+    "url": "/react-deploy/static/css/main.db4b23e7.chunk.css"
+  },
+  {
+    "revision": "f18e0527af61d6d1aafe",
+    "url": "/react-deploy/static/js/2.b90f2744.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-deploy/static/js/2.b90f2744.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c826c36fea8f693adde9",
+    "url": "/react-deploy/static/js/main.64e4e14c.chunk.js"
+  },
+  {
+    "revision": "36b35ee5c2969d3c6dab",
+    "url": "/react-deploy/static/js/runtime-main.76042ac1.js"
+  },
+  {
+    "revision": "804fa877c269e7907b7f221fc8576bb6",
+    "url": "/react-deploy/static/media/burger.804fa877.png"
+  }
+]);
